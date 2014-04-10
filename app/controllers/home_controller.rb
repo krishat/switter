9e6cc00dd84f43c58ba036bcdb1ec1d5dbd43596
krishat <akrish2011@mail.fresnostate.edu>
2014-04-10 11:59:19 -0700

    added statements to remove unwanted information from tweet to be properly paresed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -38,29 +38,51 @@ class HomeController < ApplicationController
        puts "*************************************"
        puts "searching for tweets"
        puts "*************************************"
-       puts searchterm
+       puts "searchterm:"+searchterm
+       puts "*************************************"
        #twitter stream ,:result_type => "recent"
-       @tweetinput = client.search(searchterm,:lang => "en",:result_type => "mixed").take(15)
+       @tweetinput = client.search(searchterm,:lang => "en",:result_type => "mixed").take(10)
              
        #Algorithm for performing sentimental analysis on tweets
        @tweetinput.each do |eachtweet|
        
-       @q = {}
+       
        #removing all http RT and user name from the original tweet --> modified tweet
        originaltweet = eachtweet.full_text
        split_originaltweet = originaltweet.split()
+       #puts split_originaltweet.class
        #removing using delete
        split_originaltweet.each do |orgtweet|
-         if orgtweet == /^RT|^http$/i then
-           split_originaltweet.delete orgtweet
+         
+         #remove RT keyword from tweet
+         if orgtweet.include? "RT" then
+           rtindex = split_originaltweet.index(orgtweet)
+           split_originaltweet.delete_at(rtindex)
          end
+         
+         #remove http links in the tweet
+         if orgtweet.include? "http" then
+           httpindex = split_originaltweet.index(orgtweet)  
+           split_originaltweet.delete_at(httpindex)
+         end
+         #remove #tags from the tweet
+         if orgtweet.include? "#" then
+           hashindex = split_originaltweet.index(orgtweet)
+           split_originaltweet[hashindex].slice!(0)
+         end
+         
+         if orgtweet.include? "@" then
+           atindex = split_originaltweet.index(orgtweet)
+           split_originaltweet[atindex].class
+         end        
+       
        end
        
        #generating modified tweet
        modifiedtweet = split_originaltweet.join(' ')
        text = modifiedtweet
        puts text
-       #puts text.class
+       puts "----------------------------------------"
        
        #text processing 
        text = StanfordCoreNLP::Annotation.new(text)
@@ -94,18 +116,18 @@ class HomeController < ApplicationController
                       tokenstring = token.get(:value).to_s
                       pos = token.get(:part_of_speech).to_s
                       #constructions of nodes
-                      puts !(searchterm.include?(tokenstring))
+                      #puts !(searchterm.include?(tokenstring))
                       if !(searchterm.include?(tokenstring)) 
                          
-                         puts "working"
+                         #puts "working"
                          if (pos == "JJ"||pos == "JJR"||pos == "JJS") 
                                 #construct a new two child nodes for the adjective word found
-                                                puts "adj"
+                          #                      puts "adj"
                          else
                            
                            newvertex = {tokenstring => {:adjective => [pos,0]}}
                            @graph << newvertex
-                           puts newvertex 
+                           #puts newvertex 
                            
                            end#to check whether adjective or not
                       end #if not root
@@ -118,7 +140,7 @@ class HomeController < ApplicationController
            end
          
          puts @graph
-         puts "##################"
+         #puts "##################"
        end
    @text = @tweetinput
    @text
